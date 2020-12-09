cb2f4a54257828d5246111f6ad1f43b56f884b10
Nikoline Nilsson Norby <Nikoline@MacBook-Air-3.local>
2020-12-09 10:58:45 +0100

    commit

--- Frontend/register.js
+++ Frontend/register.js
@@ -1,4 +1,4 @@
-var name = document.getElementById('name');
+    var name = document.getElementById('name');
     var password = document.getElementById('password')
     var birthday = document.getElementById("birthday");
     var email = document.getElementById("email");
@@ -9,36 +9,32 @@ var name = document.getElementById('name');
 //jeg opstiller en variable for hver form, og giver dem et navn jeg kan kalde
 function store() {
 //opsætter kriterer for informationerne og hvordan de skal udfyldes 
+    var name = document.getElementById("name");
     if(name.value.length == " ") {
         alert ('Udfyld venligst fornavn og efternavn')
     
-    }else if(password.value.length == " "){
-        alert ('Udfyldt venligst password')
+    } var password = document.getElementById("password");
+    if (password.value == "" || password.value.length < 7) {
+        alert("Password skal indeholde 7 karakterer")
 
-    }else if(password.value.length < 7){
-        alert ('Password skal være mindst 7 karakterer langt')
-    
-    }else if(birthday.value == 0){
+    } var birthday = document.getElementById("birthday");
+    if(birthday.value == 0){
         alert ('Vælg venligst fødselsdato')
     
-    }else if(email.value.length == " "){
+    } var email = document.getElementById("email");
+    if(email.value.length == " "){
         alert ('Udfyld venligst email')
     
-    }else if (gender.document.getElementsByName('gender')[0].value == '' ){
-        alert('Vælg dit køn');
-
-    }else if (interest.document.getElementsByName('interest')[0].value == '' ){
-        alert ('Vælg interesse')
-    
-    }else{
+    {
         localStorage.setItem('name', name.value);
         localStorage.setItem('email', email.value);
         localStorage.setItem('password', password.value);
-        localStorage.setItem('gender', gender1.value, gender2.value, gender3.value);
-        localStorage.setItem('interest', interest1.value, interest2.value, interst3.value, interest4.value);
+        localStorage.setItem('gender', gender.value);
+        localStorage.setItem('interest', interest.value);
             alert('Your account has been created')
-            document.getElementById("reg-btn").onclick = store(); {
+            document.getElementById("regbtn").onclick = store(); {
                 location.href = "../Frontend/profil.html";
+            }
         }
     }
 }
